50a9277bfdae0b2813d3225aea82d7877cdc986a
ying1488 <yingecteach@gmail.com>
2024-03-11 23:24:03 +1100

    first commit

---
+++ client/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Header
+      </div>
+    );
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+
+import App from './components/App'
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
