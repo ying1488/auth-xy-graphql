104460ea004092d35b7fb67650eb2b7f17f031d6
ying1488 <yingecteach@gmail.com>
2024-03-18 15:48:06 +1100

    added requireAuth as HOC reviewed auth flow

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-  render() {
-    console.log(this.props.data);
+  renderButtons() {
+    const { loading, user } = this.props.data;
 
+    if (this.props.data.loading) { return <div />; }
+
+    if (user) {
+      return (<div>Logout</div>);
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+
+  }
+
+  render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="new-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
